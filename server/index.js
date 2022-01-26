d11b1f0328e1e4f2521e75bf0cb181275c0c4e96
Cory Owen <cjowen14@outlook.com>
2022-01-26 14:53:02 -0700

    Another

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
   })
 
-app.get('/', (req, res) => {
+app.get('/js', (req, res) => {
     res.sendFile(path.join(__dirname, '../styles.css'))
   })
 
