b01edea7661b97441b249e6bf5176a9b742d061e
Cory Owen <cjowen14@outlook.com>
2022-01-26 14:24:47 -0700

    You know the drill

--- server/index.js
+++ server/index.js
@@ -9,7 +9,14 @@ app.get('/js', (req, res) => {
     res.sendFile(path.join(__dirname, './public/main.js'))
   })
 
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/main.js'))
+  })
+
 app.use(express.json());
+
+app.use('/js', express.static(path.join(__dirname, 'public/main.js')));
+app.use('/css', express.static(path.join(__dirname, 'public/main.js')));
 
 
 app.listen(port, () => {
