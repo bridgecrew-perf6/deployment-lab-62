160a4f33cc8356aea94d13d4dd6fa6875dc76ceb
Cory Owen <cjowen14@outlook.com>
2022-01-26 14:29:55 -0700

    Back to just HTML

--- server/index.js
+++ server/index.js
@@ -6,14 +6,14 @@ const port = process.env.PORT;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/main.js'))
+    res.sendFile(path.join(__dirname, '../index.html'))
   })
 
 
 app.use(express.json());
 
 app.use('/js', express.static(path.join(__dirname, 'public/main.js')));
-app.use('/css', express.static(path.join(__dirname, 'public/main.js')));
+
 
 
 app.listen(port, () => {
