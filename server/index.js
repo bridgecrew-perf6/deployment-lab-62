9efeeeb7fa77c75918c30e95563b52f38d1711c5
Cory Owen <cjowen14@outlook.com>
2022-01-26 13:25:09 -0700

    Fix 1

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'));
+    res.sendFile(path.join(__dirname, './index.html'));
 })
 
 app.use(express.json());
