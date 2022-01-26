b870516a3553ae72c4b176680f86afeb1f861672
Cory Owen <cjowen14@outlook.com>
2022-01-26 14:34:12 -0700

    Another attempt

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 
 app.use(express.json());
 
-app.use('/js', express.static(path.join(__dirname, 'public/main.js')));
+app.use('/css', express.static(path.join(__dirname, 'styles.css')));
 
 
 
