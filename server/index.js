939c18d3d5fc7257aa1267deb7605c561822a63f
Cory Owen <cjowen14@outlook.com>
2022-01-26 13:56:13 -0700

    Figuring out CSS

--- server/index.js
+++ server/index.js
@@ -5,20 +5,19 @@ const port = process.env.PORT;
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/js', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
 })
 
-app.get('/', (req, res) => {
+app.get('/js', (req, res) => {
     res.sendFile(path.join(__dirname, '../styles.css'));
 })
 
-
 app.use(express.json());
 
-app.use('/js', express.static(path.join(__dirname, 'public/main.js')));
+// app.use('/js', express.static(path.join(__dirname, 'public/main.js')));
 
-app.use('/css', express.static(path.join(__dirname, 'public/main.js')));
+// app.use('/css', express.static(path.join(__dirname, 'public/main.js')));
 
 app.listen(port, () => {
     console.log('Running on port ' + port);
